097be79e70e845faf92dd1673e1712638795ab7b
Tommy <86784639+tommy88520@users.noreply.github.com>
2022-01-01 14:59:55 +0800

    redux

---
+++ my-app/src/SeasonDisplay.js
@@ -0,0 +1,41 @@
+import './SeasonDisplay.scss'
+import React from 'react'
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake',
+  },
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat > 0 ? 'winter' : 'summer'
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const { text, iconName } = seasonConfig[season]
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      {/* <h1>{text}</h1> */}
+      <h2>
+        <div href="" data-content="Link Hover Effect 7" className="effect">
+          <span>Link Hover Effect 7</span>
+        </div>
+      </h2>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  )
+}
+
+export default SeasonDisplay
