6c72ce0f16323c066edf6ef295f6115982fdd782
Tommy <86784639+tommy88520@users.noreply.github.com>
2022-01-05 11:12:10 +0800

    第一次redux

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react'
-import actions from '../actions'
+import SongList from './SongList'
+import SongDetail from './SongDetail'
+
 const App = () => {
-  return <div>APP</div>
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  )
 }
+
 export default App
